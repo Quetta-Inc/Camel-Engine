42d258c7ec57246185b2118105deb2fb337c31f3
manwinano <rodya.karpukhin@gmail.com>
2026-09-10 22:31:34 +0300

    First program with init VULKAN lib (#2)
    * first program
    
    * first program (init Vulkan)
    
    * fix #1
    
    * fix #2
    
    * fix #3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+#include <vulkan/vulkan.h>
+#include <SDL3/SDL.h>
+
 int main() {
-    std::cout << "Hello, World!\n";
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // FIXME: use SDL_WINDOW_VULKAN
+    SDL_Window* window = SDL_CreateWindow("Camel Engine", 800, 600, NULL);
+
+    if (!window) {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+
 }
